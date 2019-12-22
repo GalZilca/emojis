383aafed41de5a64a3db808fb456838a275e6b12
GalZilca <galzilca@gmail.com>
2019-12-22 13:37:31 +0200

    no message

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { HashRouter as Router, Link, Route, Switch } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import About from "./About";
+import registerServiceWorker from "./registerServiceWorker";
+import 'semantic-ui-css/semantic.min.css'
+
+ReactDOM.render(
+  <Router>
+    <div>
+      <Link to="/">Home</Link>
+      <Link to="/About">About</Link>
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route path="/About" component={About} />
+      </Switch>
+    </div>
+  </Router>,
+  document.getElementById("root")
+);
+registerServiceWorker();
